47f794b96778e7b6ab1657396028eb147c36e2df
yassine77 <kchaou.yessin@gmail.com>
2024-04-07 12:28:58 +0200

    Responsive design for about page

--- src/sections/WhoWeAreSection.vue
+++ src/sections/WhoWeAreSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="whoareWeSection container text-center my-5 ">
-    <h1 class="sectionTitle text-orange fw-bold text-uppercase">About UBIBAT</h1>
-    <div class="content text-center fs-5 fw-bold py-3">
+    <h1 class="sectionTitle text-orange fw-sm-normal fw-bold text-uppercase">About UBIBAT</h1>
+    <div class="content text-center fw-bold fs-6 py-3">
       <p>UBIBAT est une entreprise spécialisée dans le domaine du calcul de structures, du
       diagnostic et de la reconnaissance des structures existantes et du suivi des chantiers.<br>
       Fondée par des ingénieurs spécialisés dans le domaine des calculs de structures, UBIBAT
@@ -25,14 +25,22 @@
 <style lang="scss">
 .whoareWeSection{
   .content{
-    line-height: 35px;
+    line-height: 28px;
   }
 }
 
-.sectionTitle {
-    font-size: 60px;
-    position: relative;
-    display: inline-block;
+// Medium devices
+@media (min-width: 768px) {
+  .whoareWeSection .content{
+    line-height: 30px
+  }
+}
+
+// Large devices
+@media (min-width: 992px) {
+  .whoareWeSection .content{
+    line-height: 35px;
+  }
 }
 
 // TODO
